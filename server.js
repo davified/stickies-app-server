2cbc9ccd2b37e7c8ea34a812b4259e7f15ef1b55
David Tan <davidtan@thoughtworks.com>
2017-03-21 15:44:02 +0800

    Minor linting

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const router = require('./router')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -11,8 +10,6 @@ dotenv.load()
 // Mounting middleware
 app.use(require('morgan')('dev'))
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 // error handling
@@ -30,7 +27,7 @@ var io = require('socket.io')(server)
 var numberOfConnections = 0
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html')
+  res.sendfile(path.join(__dirname, '/index.html'))
 })
 
 // 1. socket.io event #1 - connection
@@ -41,7 +38,7 @@ io.on('connection', function (socket) {
 
   // 2. socket.io event #2 - creating rooms
   socket.on('createRoom', function (data) {
-    roomName = data.channelName
+    let roomName = data.channelName
     socket.join(roomName)
 
     // Send this event to everyone in the room.
@@ -51,8 +48,8 @@ io.on('connection', function (socket) {
 
   // 3. socket.io event #3 - sending messages in the room
   socket.on('sendMessage', function (data) {
-    message = data.message
-    room = data.room
+    let message = data.message
+    let room = data.room
     io.to(room).emit('broadcastMessageToRoom', message)
   })
 
