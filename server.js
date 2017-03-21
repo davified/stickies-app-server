0a0d515ff32e860915c038b8d5611e0e1c828a79
David Tan <davidtan@thoughtworks.com>
2017-03-21 11:23:00 +0800

    Create express server and install cors and dotenv

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const router = require('./config/routes')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+dotenv.load()
+
+// Mounting middleware
+app.use(require('morgan')('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// wiring up the router
+app.use('/', router)
+
+// error handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: err
+  })
+})
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`)
+})
